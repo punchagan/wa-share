3c4ebc6f1920a40d2e5d6e6da2a32b3e2cb8b02d
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-02-26 12:08:03 +0530

    Format service worker with Prettier

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-self.addEventListener('install', event => {
-  console.log('👷', 'install', event);
+self.addEventListener("install", event => {
+  console.log("👷", "install", event);
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('👷', 'activate', event);
+self.addEventListener("activate", event => {
+  console.log("👷", "activate", event);
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   // Regular requests not related to Web Share Target.
   if (event.request.method !== "POST") {
     event.respondWith(fetch(event.request));
@@ -16,15 +16,17 @@ self.addEventListener("fetch", (event) => {
   }
 
   // Redirect to the form page
-  event.respondWith(Response.redirect('/'));
+  event.respondWith(Response.redirect("/"));
 
   // Requests related to Web Share Target.
   event.waitUntil(
     (async () => {
       const formData = await event.request.formData();
-      const client = await self.clients.get(event.resultingClientId || event.clientId);
-      const files = formData.getAll('chat');
-      client.postMessage({ files: files, action: 'chat' });
+      const client = await self.clients.get(
+        event.resultingClientId || event.clientId
+      );
+      const files = formData.getAll("chat");
+      client.postMessage({ files: files, action: "chat" });
     })()
   );
 });
