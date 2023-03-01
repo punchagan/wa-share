f0b5903faff3c0980d56398ae83f8b7e5a71feea
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-03-01 08:09:52 +0530

    Cache PWA resources to support offline usage of the app

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
+const cacheName = "wa-share-V1";
 self.addEventListener("install", event => {
   console.log("👷", "install", event);
   self.skipWaiting();
+  const contentToCache = ["/", "/index.html", "/util.js", "/logo.png"];
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
 });
 
 self.addEventListener("activate", event => {
@@ -11,7 +20,30 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   // Regular requests not related to Web Share Target.
   if (event.request.method !== "POST") {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+      (async () => {
+        console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+        let r, response;
+        try {
+          response = await fetch(event.request);
+        } catch {
+          r = await caches.match(event.request);
+        }
+        if (response) {
+          const cache = await caches.open(cacheName);
+          console.log(
+            `[Service Worker] Caching new resource: ${event.request.url}`
+          );
+          cache.put(event.request, response.clone());
+        } else if (r) {
+          console.log(
+            `[Service Worker] Using cached resource: ${event.request.url}`
+          );
+          response = r;
+        }
+        return response;
+      })()
+    );
     return;
   }
 
